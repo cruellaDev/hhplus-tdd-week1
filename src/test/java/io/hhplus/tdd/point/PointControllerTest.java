f78d7aab9d846a5cde6d0e74a9b113a1d4994530
cruellaDev <cruellaDevLee@gmail.com>
2024-06-19 01:12:42 +0900

    PointControllerTest commit.

--- src/test/java/io/hhplus/tdd/point/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -1,23 +1,26 @@
 package io.hhplus.tdd.point;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.anyLong;
+import static org.mockito.BDDMockito.given;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(PointController.class)
-@AutoConfigureMockMvc
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class PointControllerTest {
 
     @Autowired
@@ -25,6 +28,9 @@ public class PointControllerTest {
 
     @Autowired
     private PointController pointController;
+
+    @MockBean
+    private PointService pointService;
 
     @BeforeEach
     void setUp() {
@@ -42,8 +48,7 @@ public class PointControllerTest {
      * @throws Exception
      */
     @Test
-    @DisplayName("사용자 포인트 조회 호출 status ok 확인")
-    public void call_getUserPointApi_when_status_is_ok_then_success() throws Exception {
+    void call_getUserPointApi_when_status_is_ok_then_success() throws Exception {
         checkGetMethodApiIsOkStatusAndPrintResponse("/point/{id}", 0);
     }
 
@@ -52,23 +57,51 @@ public class PointControllerTest {
      * @throws Exception
      */
     @Test
-    @DisplayName("사용자 포인트 이용 내역 조회 호출 status ok 확인")
-    public void call_getUserPointHistoryApi_when_status_is_ok_then_success() throws Exception {
+    void call_getUserPointHistoryApi_when_status_is_ok_then_success() throws Exception {
         checkGetMethodApiIsOkStatusAndPrintResponse("/point/{id}/histories", 0);
     }
 
+    /**
+     * 사용자 충전 내역 없을 시 사용자 포인트 기본값 리턴
+     * @throws Exception
+     */
     @Test
-    @DisplayName("사용자 포인트 이용 내역 조회 호출 status ok 확인")
-    public void call_getUserPointApi_when_no_one_charge_then_return_empty_userPoint() throws Exception{
-        MvcResult mvcResult = mockMvc.perform(get("/point/{id}", 0))
+    void call_getUserPointApi_when_no_one_charge_then_return_empty_userPoint() throws Exception{
+        // given
+        long id = 1;
+        UserPoint expectedUserPoint = UserPoint.empty(id);
+        given(pointService.point(anyLong())).willReturn(expectedUserPoint);
+
+        // when - then
+        MvcResult mvcResult = mockMvc.perform(get("/point/{id}", id))
                 .andExpect(status().isOk())
                 .andReturn();
 
         ObjectMapper objectMapper = new ObjectMapper();
-        UserPoint userPoint = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), UserPoint.class);
+        UserPoint realUserPoint = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), UserPoint.class);
 
-        assertEquals(userPoint.id(), 0);
-        assertEquals(userPoint.point(), 0);
-        assertEquals(userPoint.updateMillis(), 0);
+        assertEquals(expectedUserPoint.id(), realUserPoint.id());
+        assertEquals(expectedUserPoint.point(), realUserPoint.point());
+    }
+
+    /**
+     * 사용자 충전 내역 없을 시 빈 내역 리스트 리턴
+     * @throws Exception
+     */
+    @Test
+    void call_getPointHistories_when_no_one_charge_then_return_empty_list() throws Exception {
+        // given
+        long id = 1;
+        given(pointService.history(anyLong())).willReturn(List.of());
+
+        // when - then
+        MvcResult mvcResult = mockMvc.perform(get("/point/{id}/histories", id))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<PointHistory> pointHistoryList = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), List.class);
+
+        assertTrue(pointHistoryList.isEmpty());
     }
 }
